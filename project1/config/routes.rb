866413352c619ccbb316d70611d8f4ff1a66d3e7
supakjack <supakjack@gmail.com>
2022-12-05 22:36:26 +0700

    cors และเพิ่ม route

--- project1/config/routes.rb
+++ project1/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "application#hello_world"
   get 'static/home', to: 'static#home'
+  post 'static/home', to: 'static#home'
+  match 'static/home', to: 'static#home', via: [:put, :patch, :delete]
 end
