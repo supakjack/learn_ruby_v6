80d10e515d24905ffaf3ece0b12a8203fc3dcbdd
supakjack <supakjack@gmail.com>
2022-12-06 20:36:49 +0700

    crud api

---
+++ project2/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get     'users',      to: "users#index"
+  post    'users',      to: "users#create"
+  get     'users/:id',  to: "users#show" 
+  put     'users/:id',  to: "users#update"
+  patch   'users/:id',  to: "users#update"
+  delete  'users/:id',  to: "users#destroy"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
