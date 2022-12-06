1b59c4b7920330a36aea4e9ff73a8859a7f0da82
supakjack <supakjack@gmail.com>
2022-12-06 21:38:19 +0700

    Scaffold Generate

--- project2/db/schema.rb
+++ project2/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_06_125740) do
+ActiveRecord::Schema.define(version: 2022_12_06_134316) do
+
+  create_table "books", force: :cascade do |t|
+    t.text "name"
+    t.text "description"
+    t.integer "pages"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.text "title"
